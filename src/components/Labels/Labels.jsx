01bbae80ebb27c3d97eefab95ac5cae2c3dbf2a4
Martín Granados <87210695+noLevii@users.noreply.github.com>
2024-01-09 20:27:53 -0600

    Add files via upload

--- src/components/Labels/Labels.jsx
+++ src/components/Labels/Labels.jsx
@@ -4,17 +4,17 @@ import "./Labels.css";
 const Labels = () => {
   return (
     <div className="labels">
-      <div className="title">Etiquetas</div>
+      <div className="title">Alumnos</div>
       <div className="grid">
         <div className="list">
-          <div className="label">Gustavo</div>
-          <div className="label">Jair</div>
-          <div className="label">Karime</div>
+          <div className="label">Ethan Sullivan</div>
+          <div className="label">Jackson Bennett</div>
+          <div className="label">Olivia Rodriguez</div>
         </div>
         <div className="list">
-          <div className="label">Luis</div>
-          <div className="label">Martín</div>
-          <div className="label">Sebastián</div>
+          <div className="label">Liam Thompson</div>
+          <div className="label">Mason Parker</div>
+          <div className="label">Caleb Morgan</div>
         </div>
       </div>
     </div>
